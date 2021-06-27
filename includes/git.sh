e3e9aa6d3e086dce717569d192437ec373c87d88
David Weldon <dweldon@gmail.com>
2021-06-27 11:02:38 -0400

    Add git lines alias

--- includes/git.sh
+++ includes/git.sh
@@ -20,6 +20,7 @@ git.execute() {
   git config --global difftool.prompt false
   git config --global core.editor "atom --wait"
   git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %ad %C(green)%s%C(reset) %C(magenta)[%an]%C(reset)%d" --graph --date=short'
+  git config --global alias.lines '!git ls-files | xargs wc -l'
   git config --global alias.cleanup '!git remote prune origin && git gc'
   util.assertExecutable 'git'
   util.assertContains "$HOME/.gitconfig" 'hist'
